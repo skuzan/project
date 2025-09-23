c6ce2f2d2452afb7c89662c73da05dba22254fa4
Sinan KUZAN <sinan_kuzan@hotmail.com>
2025-09-23 14:59:34 +0200

    averages functions

---
+++ note/note.js
@@ -0,0 +1,17 @@
+let students = [
+  { name: "Ali", lessons: ["Math", "Science"], grades: [80, 90] },
+  { name: "Emre", lessons: ["Math", "Science"], grades: [70, 80] },
+  { name: "Hasan", lessons: ["Math", "Science"], grades: [40, 50] },
+  { name: "Selma", lessons: ["Math", "Science"], grades: [50, 90] },
+];
+
+const averages = students.map((student) => {
+  let total = 0;
+  for (let i = 0; i < student.grades.length; i++) {
+    total += student.grades[i];
+  }
+  let avg = total / student.grades.length;
+  return avg;
+});
+
+console.log(averages);
